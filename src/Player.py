7c6637e09e9060f9f554f9cff596b9745df1704c
Amaan Asim <amaanasim09@gmail.com>
2023-07-16 21:20:45 -0400

    added WASD support

--- src/Player.py
+++ src/Player.py
@@ -12,13 +12,13 @@ class Player(pygame.sprite.Sprite):
 
     def update_position(self):
         keys = pygame.key.get_pressed()
-        if keys[K_LEFT] or keys[K_a] and self.rect.x > 0:
+        if (keys[K_LEFT] or keys[K_a]) and self.rect.x > 0:
             self.rect.x -= 10
-        if keys[K_RIGHT] or keys[K_d] and self.rect.x < 352 - 67:
+        if (keys[K_RIGHT] or keys[K_d]) and self.rect.x < 352 - 67:
             self.rect.x += 10
-        if keys[K_UP] or keys[K_w] and self.rect.y > 0:
+        if (keys[K_UP] or keys[K_w]) and self.rect.y > 0:
             self.rect.y -= 10
-        if keys[K_DOWN] or keys[K_s] and self.rect.y < 640 - 67:
+        if (keys[K_DOWN] or keys[K_s]) and self.rect.y < 640 - 67:
             self.rect.y += 10
 
         
