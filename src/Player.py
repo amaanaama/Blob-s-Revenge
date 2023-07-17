660a7ec7d9c334826e118de13e75a61674ba9aec
Amaan Asim <amaanasim09@gmail.com>
2023-07-17 09:53:33 -0400

    added acceleration/friction to player

--- src/Player.py
+++ src/Player.py
@@ -9,18 +9,41 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.velX = 0
+        self.velY = 0
+        self.acc = 0.2
+        self.friction = 0.1
+        self.maxVel = 5
 
     def update_position(self):
         keys = pygame.key.get_pressed()
-        if (keys[K_LEFT] or keys[K_a]) and self.rect.x > 0:
-            self.rect.x -= 10
-        if (keys[K_RIGHT] or keys[K_d]) and self.rect.x < 352 - 67:
-            self.rect.x += 10
-        if (keys[K_UP] or keys[K_w]) and self.rect.y > 0:
-            self.rect.y -= 10
-        if (keys[K_DOWN] or keys[K_s]) and self.rect.y < 640 - 67:
-            self.rect.y += 10
 
         
+        if (keys[K_LEFT] or keys[K_a]) and self.velX > -self.maxVel:
+            self.velX -= self.acc
+        if (keys[K_RIGHT] or keys[K_d]) and self.velX < self.maxVel:
+            self.velX += self.acc
+        if (keys[K_UP] or keys[K_w]) and self.velY > -self.maxVel:
+            self.velY -= self.acc
+        if (keys[K_DOWN] or keys[K_s]) and self.velY < self.maxVel:
+            self.velY += self.acc
 
         
+        if not (keys[K_LEFT] or keys[K_RIGHT]) and not (keys[K_a] or keys[K_d]):
+            if self.velX < 0:
+                self.velX += self.friction
+            elif self.velX > 0:
+                self.velX -= self.friction
+        if not (keys[K_UP] or keys[K_DOWN]) and not (keys[K_w] or keys[K_s]):
+            if self.velY < 0:
+                self.velY += self.friction
+            elif self.velY > 0:
+                self.velY -= self.friction
+
+        
+        self.rect.x += self.velX
+        self.rect.y += self.velY
+
+        
+        self.rect.x = max(0, min(self.rect.x, 352 - 67))
+        self.rect.y = max(0, min(self.rect.y, 640 - 67))
