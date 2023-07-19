c75775e57dd3f1805201718390c43df622e82ff9
Amaan Asim <amaanasim09@gmail.com>
2023-07-19 12:00:02 -0400

    added font, score, collision, etc.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ background = pygame.image.load('data/assets/background.png')
 
 #scoreboard
 scoreboard = pygame.image.load('data/assets/scoreboard.png')
+score = -1 
 
 #spawn timer
 spawn_timer = 0
@@ -29,6 +30,12 @@ P1 = Player(176, 320)
 LE = LeftEnemy(0, -64)
 RE = RightEnemy(0, -400)
 
+def check_collision():
+    global score
+    if pygame.sprite.collide_rect(P1, LE):
+        score = 0
+    if pygame.sprite.collide_rect(P1, RE):
+        score = 0
 
 #Quits the game when the X button is pressed
 while running:
@@ -38,6 +45,11 @@ while running:
 
     P1.update_position()
 
+    if LE.rect.y == -64:
+        score += 1
+    if RE.rect.y == -64:
+        score += 1
+
     #fill screen with black
     screen.fill((0, 0, 0))
 
@@ -45,7 +57,6 @@ while running:
     screen.blit(background, (0, 0))
     screen.blit(P1.image, P1.rect)
     
-
     #draw one instance of Left_Enemy.py
     screen.blit(LE.image, LE.rect)
     LE.update_position()
@@ -54,6 +65,13 @@ while running:
     RE.update_position()
 
     screen.blit(scoreboard, (93, 25))
+
+    font = pygame.font.Font('data/assets/04B_25__.TTF', 32)
+    text = font.render(str(score), True, (255, 255, 255))
+    screen.blit(text, (180, 30))
+
+    check_collision()
+
     pygame.display.flip()
     clock.tick(60)
 
